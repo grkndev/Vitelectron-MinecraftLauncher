62e6a9f619b9baf0389c318b8b4fae934c3cdac1
grkndev <gweepcreative@gmail.com>
2025-06-19 17:43:20 +0300

    Vitelectron

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <span className='text-6xl font-bold underline'>Hello World</span>
+      </div>
+    </>
+  )
+}
+
+export default App
